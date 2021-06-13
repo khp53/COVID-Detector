d5981c38f1637c43212a2f4f436c4bd7e7ffeda8
khp53 <khp4121@gmail.com>
2021-06-13 13:18:13 +0800

    beta completed

---
+++ lib/View/home.dart
@@ -0,0 +1,9 @@
+import 'package:covid_detector/View/mobile_homepage.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MobileHomepage();
+  }
+}
